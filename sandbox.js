2ad21ab7efe3ecbfe6be77e806d2fe007389d3eb
appsius <ilyas.ustunkaya234434@gmail.com>
2020-04-08 18:09:57 +0300

    Deleting Docs

--- sandbox.js
+++ sandbox.js
@@ -1,24 +1,25 @@
 const list = document.querySelector('ul');
 const form = document.querySelector('form');
 
-const addRecipe = (recipe) => {
+const addRecipe = (recipe, id) => {
 	let time = recipe.created_at.toDate();
 	let html = `
-      <li>
+      <li data-id="${id}">
          <div>${recipe.title}</div>
          <div>${time}</div>
+         <button class="btn btn-danger btn-sm my-2">Delete</button>
       </li>
    `;
 
 	list.innerHTML += html;
 };
 
+// Get documents
 db.collection('recipes')
 	.get()
 	.then((snapshot) => {
 		snapshot.docs.forEach((doc) => {
-			console.log(doc.data());
-			addRecipe(doc.data());
+			addRecipe(doc.data(), doc.id);
 		});
 	})
 	.catch((err) => {
@@ -43,4 +44,18 @@ form.addEventListener('submit', (e) => {
 		.catch((err) => {
 			console.log(err);
 		});
+});
+
+// Deleting data
+list.addEventListener('click', (e) => {
+	// console.log(e);
+	if (e.target.tagName === 'BUTTON') {
+		const id = e.target.parentElement.getAttribute('data-id');
+		db.collection('recipes')
+			.doc(id)
+			.delete()
+			.then(() => {
+				console.log('Recipe deleted!!!');
+			});
+	}
 });
